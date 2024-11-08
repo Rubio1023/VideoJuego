b161d0ebc2a237c4303ca0afa75a87382c92c838
Kevin Rubio <Rubio1023@users.noreply.github.com>
2024-11-08 18:03:08 -0500

    estetica

--- personajes/Personajes.java
+++ personajes/Personajes.java
@@ -5,52 +5,44 @@ import armas.Armas;
 public abstract class Personajes 
 {
     private String nombre; // Nombre del personaje
-    private int HP; // Puntos de vida
-    private int MP; // Puntos de magia
+    private int HP =100; // Puntos de vida
+    private int MP =100; // Puntos de magia
     protected Armas armas;  // Referencia al arma 
     private boolean defendiendo; // Variable para saber si el personaje se está defendiendo
-
     // Constructor
     public Personajes(String nombre, int HP, int MP, Armas armas) 
     {
         this.nombre = nombre;
-        this.HP = 100;
-        this.MP = 100;
+        this.HP = HP;
+        this.MP = MP;
         this.armas = armas;
         this.defendiendo = false;
     }
-
     // Metodo para obtener el nombre del personaje, la vida, la magia y el arma
     public String getNombre() 
     {
         return this.nombre;
     }
-
     public int getHP() 
     {
         return this.HP;
     }   
-
     public int getMP() 
     {
         return this.MP;
     }
-
     public Armas getArmas() 
     {
         return this.armas;
     }
-
     // Metodos abstractos
     public abstract void atacar(Personajes enemigo);
-
     // Metodo para defenderse
     public void defender() 
     {
         this.defendiendo = true;
         System.out.println(nombre + " se prepara para defender.");
     }
-    
     // Metodo para recibir daño
     public void recibirDamage(int damage) 
     {
@@ -67,7 +59,6 @@ public abstract class Personajes
             defendiendo = false;
         }    
     }
-
     // Metodo de validacion, para verificar que este vivo
     public boolean estaVivo() 
     {
